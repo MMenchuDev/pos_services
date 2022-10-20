2375a16e0189f175c0d724b674abc641c854eb2b
Miguel Angel Menchú Xoyón <michole77@hotmail.com>
2022-10-20 10:57:51 -0600

    with cros all

--- src/main/java/com/itgt/pos/controller/ArticuloController.java
+++ src/main/java/com/itgt/pos/controller/ArticuloController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.itgt.pos.model.Articulo;
 import com.itgt.pos.model.Articulo;
 import com.itgt.pos.service.ArticuloService;
 
+@CrossOrigin //  (originPatterns = {"http://localhost:3000"}) acceso a datos React
 @RestController
 @RequestMapping("api/Articulo")
 public class ArticuloController {
--- src/main/java/com/itgt/pos/controller/PersonaController.java
+++ src/main/java/com/itgt/pos/controller/PersonaController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itgt.pos.model.Persona;
 import com.itgt.pos.service.PersonaService;
 
+@CrossOrigin //  (originPatterns = {"http://localhost:3000"}) acceso a datos React
 @RestController
 @RequestMapping("api/Persona")
 public class PersonaController {
