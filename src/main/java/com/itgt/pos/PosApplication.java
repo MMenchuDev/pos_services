24cb1732a6f30b425af485c18b76a9e86b2203e8
Miguel Angel Menchú Xoyón <michole77@hotmail.com>
2023-01-30 23:11:26 -0600

    set time zone America/guatemala

--- src/main/java/com/itgt/pos/PosApplication.java
+++ src/main/java/com/itgt/pos/PosApplication.java
@@ -1,5 +1,9 @@
 package com.itgt.pos;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +13,9 @@ public class PosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PosApplication.class, args);
 	}
-
+    @PostConstruct
+    public void init(){
+      // Setting Spring Boot SetTimeZone
+      TimeZone.setDefault(TimeZone.getTimeZone("America/Guatemala"));
+    }
 }
